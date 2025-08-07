25a4192e7cf906d1385a20dcb8673d3a15082093
alexia456 <alexiadelfinodeoliveira30@gmail.com>
2025-08-07 00:03:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -44,7 +44,8 @@ function contaFrequencias(palavras) {
 }
 
 function tiraPalavrasRuins(palavras) {
-    const PALAVRAS_RUINS = new Set(["para","uma","nós"]);
+    
+
     const palavrasBoas = [];
     for  (let palavra of palavras) {
         if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
