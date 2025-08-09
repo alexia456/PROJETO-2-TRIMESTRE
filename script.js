7b8a674f20d4d6fc814889a82920c8b4eb8aaa0e
alexia456 <alexiadelfinodeoliveira30@gmail.com>
2025-08-09 15:27:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { PALAVRAS_RUINS } from ". /palavrasRuins.js";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
@@ -44,13 +45,11 @@ function contaFrequencias(palavras) {
 }
 
 function tiraPalavrasRuins(palavras) {
-    
-
-    const palavrasBoas = [];
+     const palavrasBoas = [];
     for  (let palavra of palavras) {
         if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
             palavrasBoas.push(palavra);
         }
     }
-    return palavraBoas;
+    return palavrasBoas;
 }
